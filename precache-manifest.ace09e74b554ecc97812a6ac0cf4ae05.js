005086048e14570c88f1375cc1b639308f6c1fbc
lu <lucymerlier@gmail.com>
2021-04-26 17:12:09 +0200

    Updates

---
+++ precache-manifest.ace09e74b554ecc97812a6ac0cf4ae05.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8b2da2533804493e749b65040f81c4",
+    "url": "/php-workshop-simple-mvc/index.html"
+  },
+  {
+    "revision": "49bd68bad0f76ceeb942",
+    "url": "/php-workshop-simple-mvc/static/js/2.cfea14c7.chunk.js"
+  },
+  {
+    "revision": "93da295041acd1ecab9d",
+    "url": "/php-workshop-simple-mvc/static/js/main.a23fa1be.chunk.js"
+  },
+  {
+    "revision": "89db49cf6bc710c0b115",
+    "url": "/php-workshop-simple-mvc/static/js/runtime~main.c48635d7.js"
+  }
+]);
